166750b28ffb6b2ef5c0bc2393a689f7277f0e75
Inês Santos Carneiro <inesscarneiro94@gmail.com>
2021-11-08 15:17:02 +0000

    Update article-error-page.tsx

--- src/views/article-error-page/article-error-page.tsx
+++ src/views/article-error-page/article-error-page.tsx
@@ -1,4 +1,5 @@
 import ArticleErrorPage from "../../components/pages/article-error-page/article-error-page";
+import "./article-error-page.css";
 import "normalize.css";
 
 const ArticleErrorPageView = () => <ArticleErrorPage />;
